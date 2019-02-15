d165e1b99aefea865f5303f7a53682808450a4fa
Semin Dmitry <semin@imp-m.ru>
2019-02-15 15:28:17 +0300

    мелкие правки

--- src/components/wizard/items/WizardModal.vue
+++ src/components/wizard/items/WizardModal.vue
@@ -2,8 +2,10 @@
   <div class="modal dolz" style="position: absolute; background: black; color: white; z-index: 99; top: 0; left: 0; right: 0; bottom: 0;">
     <Button @click="showModal(false)" type="primary" class="ml12">Закрыть</Button>
 
+    <Page :total="data.length" :current="currentPage" :page-size="limit" class="mr12" @on-change="changePage"/>
+
     <div style="height: 40vh; width: 50vw; overflow-y: auto;">
-      <Table id="modalTable" :columns="columnsOptions" @on-row-dblclick="onRowDbClick" :data="dataList"></Table>
+      <Table :columns="columnsOptions" @on-row-dblclick="onRowDbClick" :data="dataList"></Table>
     </div>
 
   </div>
@@ -18,46 +20,36 @@
       columnsOptions: Array,
       data: Array,
     },
-    mounted() {
-      let modalTable = document.getElementById('modalTable');
-      if (funcUtils.isNotEmpty(modalTable)) {
-        let modalTableParent = modalTable.parentElement;
-        modalTableParent.addEventListener('scroll', this.handleScroll);
+    created() {
+      this.fillDataList();
+    },
+    data() {
+      return {
+        from: 0,
+        to: 10,
+        limit: 10,
+        currentPage: 1,
+        dataList: []
       }
     },
-    computed: {
-      dataList() {
+    methods: {
+      changePage(nextPage) {
+        let delta = 10;
+        this.to = delta * nextPage;
+        this.from = (delta * nextPage) - delta;
+        this.currentPage = nextPage;
+
+        this.fillDataList();
+      },
+      fillDataList() {
         let res = [];
-        for (let i = 0; i < this.scrollCount; i++) {
+        for (let i = this.from; i < this.to; i++) {
           let item = this.data[i];
           if (item) {
             res.push(item);
           }
         }
-        return res;
-      },
-    },
-    data() {
-      return {
-        scrollCount: 40
-      }
-    },
-    methods: {
-      handleScroll() {
-        if (this.checkScroll() && this.scrollCount < this.data.length) {
-          let count = 40;
-          if (this.data.length < this.scrollCount + count) {
-            count = this.data.length - this.scrollCount;
-          }
-          this.scrollCount += count;
-          let modalTableParent = document.getElementById('modalTable').parentElement;
-          modalTableParent.scrollTop -= 1;
-
-        }
-      },
-      checkScroll() {
-        let modalTableParent = document.getElementById('modalTable').parentElement;
-        return (modalTableParent.offsetHeight + modalTableParent.scrollTop >= modalTableParent.scrollHeight);
+        this.dataList = res;
       },
       onRowDbClick(item) {
         this.$emit('onRowDbClick', item);
