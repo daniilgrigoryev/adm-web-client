01da454569958fd9afaed9bf6e454173df2d269a
Semin Dmitry <semin@imp-m.ru>
2019-02-19 16:33:05 +0300

    мелкие правки

---
+++ src/components/InputMask.vue
@@ -0,0 +1,38 @@
+<template>
+  <Input ref="maskedField" @input="onInput" :value="value" :class="inputProps.class"></Input>
+</template>
+
+<script>
+  import Inputmask from 'inputmask';
+
+  export default {
+    name: "InputMask",
+    props: {
+      inputProps: {
+        type: Object
+      },
+      value: {
+        type: String
+      },
+      maskProps: {
+        type: Object
+      },
+    },
+    mounted () {
+      let im = new Inputmask(this.maskProps);
+      im.mask(this.$refs.maskedField.$refs.input);
+    },
+    data() {
+      return {}
+    },
+    methods: {
+      onInput(e) {
+        this.$emit('input', e);
+      },
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
