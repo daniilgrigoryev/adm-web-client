c235b5916d7f4a8bef4cc553028d521cdbd5fb23
Daniil Grigoryev <grigorev@stdpr.ru>
2019-02-20 15:57:02 +0300

    форма логина

--- src/components/Authorization.vue
+++ src/components/Authorization.vue
@@ -1,9 +1,39 @@
 <template>
-  <div>
-    <input type="text" v-model="userName" @keyup.enter.native="authorize" />
-    <input type="text" v-model="password" @keyup.enter.native="authorize" />
-    <button type="button" @click="authorize">Войти</button>
-  </div>
+  <Layout class="viewport-full">
+    <div class="flex-parent flex-parent--center-main flex-parent--center-cross viewport-full">
+        <Card class="login-card px30 py30">
+          <div class='flex-parent flex-parent--center-cross mx-auto mt30 ml30 wmax360'>
+            <div class='login-card-img'>
+              <img src="../assets/images/system-logo.png">
+            </div>
+            <div class="login-card-subtitle adm-text-big txt-bold" style="color: #797979;">
+              Информационные ресурсы<br>
+              Московской Административной<br>
+              Дорожной Инспекции
+            </div>
+          </div>
+
+          <div class="align-center my60">
+            <h2 class="adm-h2">Вход в подсистемы</h2>
+          </div>
+
+          <Form label-position="top">
+            <FormItem>
+              <small class="adm-text-small color-gray-medium" slot="label">Имя пользователя</small>
+              <Input class="adm-input adm-input--regular" size="large" type="text" v-model="userName" @keyup.enter.native="authorize" placeholder="Логин"/>
+            </FormItem>
+            <FormItem>
+              <small class="adm-text-small color-gray-medium" slot="label">Пароль</small>
+              <Input class="adm-input adm-input--regular" size="large" type="password" v-model="password" @keyup.enter.native="authorize" placeholder="Пароль"/>
+            </FormItem>
+            <div class="py12 align-center">
+              <Button class="adm-btn-primary round-full txt-uppercase" style="width: 160px; " size="large" @click="authorize" type="primary">Войти</Button>
+            </div>
+          </Form>
+        </Card>
+
+    </div> 
+  </Layout>
 
 </template>
 
@@ -80,101 +110,16 @@
   }
 </script>
 
-<style lang='scss'>
-  .section-auth {
-    display: inline-block;
-    width: 100%;
-    text-align: center;
-
-    &__line {
-      display: flex;
-      margin: 15px 0;
-      align-items: center;
-      justify-content: space-between;
-
-      &_center {
-        justify-content: center;
-      }
-    }
-
-    &__label {
-      display: inline-block;
-      width: 5vw;
-      min-width: 80px;
-      padding: 0 15px 0 0;
-      text-align: right;
-      font-size: 17px;
-      color: #7f7f7f;
-    }
-
-    &__recover {
-      font-family: "DINPro", "Helvetica Neue", Helvetica, sans-serif;
-      font-size: 12px;
-      font-weight: normal;
-      font-style: italic;
-      font-stretch: normal;
-      line-height: 1.33;
-      letter-spacing: 0.2px;
-      color: #87cc45;
-      cursor: pointer;
-    }
+<style lang='scss' scoped>
+  .login-card{
+    width: 482px;
+    box-shadow: 0 1px 6px rgba(0,0,0,.2);
   }
-
-  .auth {
-    position: relative;
-    // height: 100%;
-    height: 100vh;
-
-    &__bg {
-      z-index: 1;
-      position: absolute;
-      width: 100%;
+  .login-card-img{
+    width: 78px;
+    height: 78px;
+    img{
       height: 100%;
-      background: repeating-linear-gradient(45deg, #353535, #353535 2px, #2f2f2f 2px, #2f2f2f 10px);
-    }
-
-    &__logo {
-      display: inline-block;
-      width: 92px;
-      margin: 8vh 0 0;
-    }
-
-    &__heading {
-      margin: 100px 0 30px;
-      font-size: 22px;
-      opacity: 0.65;
-      color: #fff;
-      font-weight: 500;
-    }
-
-    &__form-wrap {
-      z-index: 2;
-      position: relative;
-      width: 25vw;
-      min-width: 420px;
-      height: 100%;
-      margin: 0 auto;
-      padding: 40px 0 30px;
-      background: #373737;
-      text-align: center;
-      align-items: center;
-      box-shadow: 0 0 15px rgba(0, 0, 0, 0.5);
-    }
-
-    &-form {
-      width: 90%;
-      margin: 0 auto;
-      text-align: center;
-
-      &__label {
-        display: inline-block;
-        width: 5vw;
-        min-width: 80px;
-        padding: 0 15px 0 0;
-        text-align: right;
-        font-size: 17px;
-        color: #7f7f7f;
-      }
     }
   }
 </style>
