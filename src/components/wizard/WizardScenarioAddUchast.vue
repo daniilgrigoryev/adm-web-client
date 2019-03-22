a8ce9f30322189f18e6ceb7a8c427098ddeabd7c
Semin Dmitry <semin@imp-m.ru>
2019-03-22 16:24:38 +0300

    мелкие правки

--- src/components/wizard/WizardScenarioAddUchast.vue
+++ src/components/wizard/WizardScenarioAddUchast.vue
@@ -87,6 +87,8 @@
         let resp =  JSON.parse(eventResponse.response);
         if (resp.error && resp.error.errorMsg) {
           alert(resp.error.errorMsg);
+        } else {
+          this.getPrev();
         }
       },
       getPrev() {
