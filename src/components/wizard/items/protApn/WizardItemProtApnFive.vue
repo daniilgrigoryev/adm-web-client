08bd5320bc96a4dbe03230874aefdd457281f095
Semin Dmitry <semin@imp-m.ru>
2019-04-08 11:27:25 +0300

    мелкие правки

--- src/components/wizard/items/protApn/WizardItemProtApnFive.vue
+++ src/components/wizard/items/protApn/WizardItemProtApnFive.vue
@@ -3,6 +3,14 @@
     <wizard-modal v-if="rasmModal.visible" :columnsOptions="rasmModal.columnsOptions" :data="rasmModal.rasmList" @showModal="showRasmModal" @onRowDbClick="onRasmClick"></wizard-modal>
 
     <div class="adm-form__item">
+      <small class="adm-form__label">Дата рассмотрения</small>
+      <Row :gutter="16" type="flex" align="middle">
+        <Col :xs="24" :md="14" :lg="16">
+          <DatePickerMask class="adm-input adm-input--regular wmin120 wmax180" v-model="data.dateRasm" @change="storeElementData" clearable type="date" placeholder="дд/мм/гггг" momentFormat="DD/MM/YYYY" maskFormat="dd/mm/yyyy"></DatePickerMask>
+        </Col>
+      </Row>
+    </div>
+    <div class="adm-form__item">
       <small class="adm-form__label">Подразделение</small>
       <div class="adm-form__item_content">
         <Row :gutter="16" type="flex" align="middle">
