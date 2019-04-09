529b33b8c4a22be78ae4e49a478917a11e7e6ba9
Semin Dmitry <semin@imp-m.ru>
2019-04-09 17:03:54 +0300

    мелкие правки

---
+++ src/components/mixins/post/wizardItemDocPostFirstMethods.js
@@ -0,0 +1,435 @@
+import * as funcUtils from "../../../assets/js/utils/funcUtils";
+import * as formStack from '../../../assets/js/api/formStack';
+import RequestApi from "../../../assets/js/api/requestApi";
+
+export default {
+  data() {
+    return {
+      dolzModal: {
+        visible: false,
+        sispList: null,
+        columnsOptions:
+          [
+            {
+              title: 'Нагрудный знак',
+              key: 'inspKod',
+              minWidth: 120,
+              ellipsis: true,
+              sortable: 'custom',
+              filterable: true,
+              tooltip: true,
+              renderHeader: (h, params) => {
+                return h('h4', params.column.title)
+              }
+            },
+            {
+              title: 'ФИО',
+              key: 'inspName',
+              minWidth: 120,
+              ellipsis: true,
+              sortable: 'custom',
+              filterable: true,
+              tooltip: true,
+              renderHeader: (h, params) => {
+                return h('h4', params.column.title)
+              }
+            },
+            {
+              title: 'Должность',
+              key: 'inspDolz',
+              minWidth: 120,
+              ellipsis: true,
+              sortable: 'custom',
+              filterable: true,
+              tooltip: true,
+              renderHeader: (h, params) => {
+                return h('h4', params.column.title)
+              }
+            },
+            {
+              title: 'Звание',
+              key: 'inspRang',
+              minWidth: 120,
+              ellipsis: true,
+              sortable: 'custom',
+              filterable: true,
+              tooltip: true,
+              renderHeader: (h, params) => {
+                return h('h4', params.column.title)
+              }
+            },
+            {
+              title: 'Код подразделения',
+              key: 'organKod',
+              minWidth: 120,
+              ellipsis: true,
+              sortable: 'custom',
+              filterable: true,
+              tooltip: true,
+              renderHeader: (h, params) => {
+                return h('h4', params.column.title)
+              }
+            },
+            {
+              title: 'Подразделение',
+              key: 'ogaiName',
+              minWidth: 120,
+              ellipsis: true,
+              sortable: 'custom',
+              filterable: true,
+              tooltip: true,
+              renderHeader: (h, params) => {
+                return h('h4', params.column.title)
+              }
+            },
+            {
+              title: 'Отдел',
+              key: 'otdName',
+              minWidth: 120,
+              ellipsis: true,
+              sortable: 'custom',
+              filterable: true,
+              tooltip: true,
+              renderHeader: (h, params) => {
+                return h('h4', params.column.title)
+              }
+            },
+            {
+              title: 'Телефон',
+              key: 'phone',
+              minWidth: 120,
+              ellipsis: true,
+              sortable: 'custom',
+              filterable: true,
+              tooltip: true,
+              renderHeader: (h, params) => {
+                return h('h4', params.column.title)
+              }
+            },
+            {
+              title: 'Дата начала действия',
+              key: 'dateBeg',
+              minWidth: 120,
+              ellipsis: true,
+              sortable: 'custom',
+              filterable: true,
+              tooltip: true,
+              renderHeader: (h, params) => {
+                return h('h4', params.column.title)
+              }
+            },
+            {
+              title: 'Дата окончания действия',
+              key: 'dateEnd',
+              minWidth: 120,
+              ellipsis: true,
+              sortable: 'custom',
+              filterable: true,
+              tooltip: true,
+              renderHeader: (h, params) => {
+                return h('h4', params.column.title)
+              }
+            }
+          ]
+      },
+      organModal: {
+        visible: false,
+        gibddList: null,
+        columnsOptions:
+          [
+            {
+              title: 'Код органа',
+              key: 'ORGAN_KOD',
+              minWidth: 120,
+              ellipsis: true,
+              sortable: 'custom',
+              filterable: true,
+              tooltip: true,
+              renderHeader: (h, params) => {
+                return h('h4', params.column.title)
+              }
+            },
+            {
+              title: 'Код региона',
+              key: 'RESP_KOD',
+              minWidth: 120,
+              ellipsis: true,
+              sortable: 'custom',
+              filterable: true,
+              tooltip: true,
+              renderHeader: (h, params) => {
+                return h('h4', params.column.title)
+              }
+            },
+            {
+              title: 'Регион',
+              key: 'REGION_NAME',
+              minWidth: 120,
+              ellipsis: true,
+              sortable: 'custom',
+              filterable: true,
+              tooltip: true,
+              renderHeader: (h, params) => {
+                return h('h4', params.column.title)
+              }
+            },
+            {
+              title: 'Район',
+              key: 'RAYON_NAME',
+              minWidth: 120,
+              ellipsis: true,
+              sortable: 'custom',
+              filterable: true,
+              tooltip: true,
+              renderHeader: (h, params) => {
+                return h('h4', params.column.title)
+              }
+            },
+            {
+              title: 'Тип',
+              key: 'TIP',
+              minWidth: 120,
+              ellipsis: true,
+              sortable: 'custom',
+              filterable: true,
+              tooltip: true,
+              renderHeader: (h, params) => {
+                return h('h4', params.column.title)
+              }
+            },
+            {
+              title: 'Название',
+              key: 'ORGAN_NAME',
+              minWidth: 120,
+              ellipsis: true,
+              sortable: 'custom',
+              filterable: true,
+              tooltip: true,
+              renderHeader: (h, params) => {
+                return h('h4', params.column.title)
+              }
+            },
+            {
+              title: 'Контакты',
+              key: 'CONTACTS',
+              minWidth: 120,
+              ellipsis: true,
+              sortable: 'custom',
+              filterable: true,
+              tooltip: true,
+              renderHeader: (h, params) => {
+                return h('h4', params.column.title)
+              }
+            },
+            {
+              title: 'Адрес',
+              key: 'KA_ADR_FULL',
+              minWidth: 120,
+              ellipsis: true,
+              sortable: 'custom',
+              filterable: true,
+              tooltip: true,
+              renderHeader: (h, params) => {
+                return h('h4', params.column.title)
+              }
+            }
+          ]
+      }
+    }
+  },
+  methods: {
+
+    async createNewUIN() {
+      let eventResponse = await RequestApi.prepareData({
+        method: 'invokeElementMethod',
+        params: {
+          eCID: this.info.eCID,
+          methodName: 'createNewUIN',
+          data: null
+        }
+      });
+      let data = JSON.parse(JSON.parse(eventResponse.response).data);
+      if (funcUtils.isEmpty(data)) {
+        let error = JSON.parse(eventResponse.response).error.errorMsg;
+        alert(error);
+      } else {
+        this.data = data;
+      }
+    },
+    async createNewDeloNum() {
+      let eventResponse = await RequestApi.prepareData({
+        method: 'invokeElementMethod',
+        params: {
+          eCID: this.info.eCID,
+          methodName: 'createDeloNum',
+          data: null
+        }
+      });
+      let data = JSON.parse(JSON.parse(eventResponse.response).data);
+      if (funcUtils.isEmpty(data)) {
+        let error = JSON.parse(eventResponse.response).error.errorMsg;
+        alert(error);
+      } else {
+        this.data = data;
+      }
+    },
+
+    async showDolzModal(visible) {
+      if (visible) {
+        let eventResponse = await RequestApi.prepareData({
+          method: 'invokeElementMethod',
+          params: {
+            eCID: this.info.eCID,
+            methodName: 'getSinspList',
+            data: JSON.stringify({
+              inspKod: this.data.inspSostKod
+            })
+          }
+        });
+        this.dolzModal.sispList = JSON.parse(JSON.parse(eventResponse.response).data);
+      }
+      this.dolzModal.visible = visible;
+    },
+    async showOrganModal(visible) {
+      if (visible) {
+        let eventResponse = await RequestApi.prepareData({
+          method: 'invokeElementMethod',
+          params: {
+            eCID: this.info.eCID,
+            methodName: 'getGibddDict',
+            data: JSON.stringify({
+              organKod: this.data.organSostKod
+            })
+          }
+        });
+        this.organModal.gibddList = JSON.parse(JSON.parse(eventResponse.response).data);
+      }
+      this.organModal.visible = visible;
+    },
+
+    async changeOrganSostKod() {
+      if (funcUtils.isNotEmpty(this.data.organSostKod)) {
+        let eventResponse = await RequestApi.prepareData({
+          method: 'invokeElementMethod',
+          params: {
+            eCID: this.info.eCID,
+            methodName: 'getGibddDict',
+            data: JSON.stringify({
+              organKod: this.data.organSostKod
+            })
+          }
+        });
+        this.clearOrganSost();
+        let gibddList = JSON.parse(JSON.parse(eventResponse.response).data);
+        if (gibddList.length > 0) {
+          this.organModal.visible = true;
+          this.organModal.gibddList = gibddList;
+        }
+      } else {
+        this.clearOrganSost();
+      }
+    },
+    async changeInspSostKod() {
+      if (funcUtils.isNotEmpty(this.data.inspSostKod)) {
+        let eventResponse = await RequestApi.prepareData({
+          method: 'invokeElementMethod',
+          params: {
+            eCID: this.info.eCID,
+            methodName: 'getSinspList',
+            data: JSON.stringify({
+              inspKod: this.data.inspSostKod
+            })
+          }
+        });
+        this.clearInspSost();
+        let data = JSON.parse(JSON.parse(eventResponse.response).data);
+        if (funcUtils.isNotEmpty(data) && data.length > 0) {
+          data = data[0];
+          this.data.inspSostId = data.inspId;
+          this.data.inspSostKod = data.inspKod;
+          this.data.inspSostName = data.inspName;
+          this.data.inspSostDolz = data.inspDolz;
+          this.data.inspSostRang = data.inspRang;
+          this.data.organSostId = data.ogaiId;
+          this.data.organSostKod = data.organKod;
+          this.data.organSostName = data.ogaiName;
+          this.storeElementData();
+        }
+      } else {
+        this.clearInspSost();
+      }
+    },
+    changeFIO() {
+      let fioLength = 0;
+      let fioArr = this.data.inspSostName.split(' ');
+      this.data.inspSostName = '';
+      for (let i = 0; i < fioArr.length && fioLength < 3; i++) {
+        let express = /^[а-яА-ЯёЁ]+$/;
+        let item = fioArr[i];
+        if (item.length > 0 && express.test(item)) {
+          switch (fioLength) {
+            case 0:
+            {
+              this.data.inspSostName += item;
+              break;
+            }
+            case 1:
+            {
+              this.data.inspSostName += ' ' + item;
+              break;
+            }
+            case 2:
+            {
+              this.data.inspSostName += ' ' + item;
+              break;
+            }
+          }
+          fioLength++;
+        }
+      }
+      this.clearInspSostKod();
+    },
+
+    onSispClick(data) {
+      this.data.inspSostId = data.inspId;
+      this.data.inspSostKod = data.inspKod;
+      this.data.inspSostName = data.inspName;
+      this.data.inspSostDolz = data.inspDolz;
+      this.data.inspSostRang = data.inspRang;
+      this.data.organSostId = data.ogaiId;
+      this.data.organSostKod = data.organKod;
+      this.data.organSostName = data.ogaiName;
+      this.dolzModal.visible = false;
+      this.dolzModal.sispList = null;
+      this.storeElementData();
+    },
+    onGibddClick(data) {
+      this.data.organSostId = data.ID;
+      this.data.organSostKod = data.ORGAN_KOD;
+      this.data.organSostName = data.ORGAN_NAME;
+      this.organModal.gibddList = null;
+      this.organModal.visible = false;
+      this.storeElementData();
+    },
+
+    clearInspSostKod() {
+      this.data.inspSostId = null;
+      this.data.inspSostKod = null;
+      this.storeElementData();
+    },
+    clearInspSost() {
+      this.data.inspSostId = null;
+      this.data.inspSostKod = null;
+      this.data.inspSostName = null;
+      this.data.inspSostDolz = null;
+      this.data.inspSostRang = null;
+      this.storeElementData();
+    },
+    clearOrganSost() {
+      this.data.organSostId = null;
+      this.data.organSostKod = null;
+      this.data.organSostName = null;
+      this.storeElementData();
+    },
+  }
+}
---
+++ src/components/mixins/post/wizardItemDocPostSecondMethods.js
@@ -0,0 +1,82 @@
+import * as funcUtils from "../../../assets/js/utils/funcUtils";
+import * as formStack from '../../../assets/js/api/formStack';
+import RequestApi from "../../../assets/js/api/requestApi";
+
+export default {
+  data() {
+    return {
+      pnpaList: null,
+      stotvSearchInfoList: null,
+      KBKSearchInfoList: null,
+    }
+  },
+  methods: {
+    async fillPnpaList() {
+      let eventResponse = await RequestApi.prepareData({
+        method: 'invokeElementMethod',
+        params: {
+          eCID: this.info.eCID,
+          methodName: 'getPnpaDict',
+          data: null
+        }
+      });
+      let pnpaList = [];
+      let pnpaDict = JSON.parse(JSON.parse(eventResponse.response).data);
+      for (let i = 0; i < pnpaDict.length; i++) {
+        let pnpa = pnpaDict[i];
+        pnpaList.push({
+          label: pnpa.PNPA_NAME,
+          value: pnpa.PNPA_KOD,
+          id: pnpa.PNPA_ID
+        });
+      }
+      this.pnpaList = pnpaList;
+    },
+    async fillStotvSearchInfo() {
+      let eventResponse = await RequestApi.prepareData({
+        method: 'invokeElementMethod',
+        params: {
+          eCID: this.info.eCID,
+          methodName: 'getStotvSearchInfo',
+          data: JSON.stringify({
+            date: this.data.dateNar
+          })
+        }
+      });
+      let stotvSearchInfoList = [];
+      let stotvSearchInfoDict = JSON.parse(JSON.parse(eventResponse.response).data);
+      for (let i = 0; i < stotvSearchInfoDict.length; i++) {
+        let stotvSearchInfo = stotvSearchInfoDict[i];
+        stotvSearchInfoList.push({
+          label: stotvSearchInfo.stotvName,
+          value: stotvSearchInfo.stotvKod,
+          id: stotvSearchInfo.stotvId
+        });
+      }
+      this.stotvSearchInfoList = stotvSearchInfoList;
+    },
+    async fillKBKSearchInfo() {
+      let eventResponse = await RequestApi.prepareData({
+        method: 'invokeElementMethod',
+        params: {
+          eCID: this.info.eCID,
+          methodName: 'getKBKSearchInfo',
+          data: JSON.stringify({
+            stotvId: this.data.stotvId
+          })
+        }
+      });
+      let KBKSearchInfoList = [];
+      let KBKSearchInfoDict = JSON.parse(JSON.parse(eventResponse.response).data);
+      for (let i = 0; i < KBKSearchInfoDict.length; i++) {
+        let KBKSearchInfo = KBKSearchInfoDict[i];
+        KBKSearchInfoList.push({
+          label: KBKSearchInfo.kbkName,
+          value: KBKSearchInfo.kbk,
+          id: KBKSearchInfo.id
+        });
+      }
+      this.KBKSearchInfoList = KBKSearchInfoList;
+    },
+  }
+}
