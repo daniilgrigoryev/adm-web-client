d979d9c7dd5658f4f1220abb75b72bcf730c794c
Daniil Grigoryev <grigorev@stdpr.ru>
2019-04-15 11:41:23 +0300

    маска цвета возможность добавлять пробелы

--- src/components/wizard/items/protApn/WizardItemVehsApn.vue
+++ src/components/wizard/items/protApn/WizardItemVehsApn.vue
@@ -66,9 +66,7 @@
               <Row :gutter="16" type="flex" align="middle">
                 <Col :xs="24" :md="14" :lg="16">
                   <!-- TODO -->
-                  <masked-input inputClass="adm-input adm-input--regular wmax240 wmin180" v-model="data.regdoc"
-                                :maskProps="{casing: 'upper', regex: '[a-zA-Zа-яА-Я0-9]+', placeholder: ''}"
-                                @onInputChange="storeElementData"></masked-input>
+                  <masked-input inputClass="adm-input adm-input--regular wmax240 wmin180" v-model="data.regdoc" :maskProps="{casing: 'upper', regex: '[a-zA-Zа-яА-Я0-9]+', placeholder: ''}" @onInputChange="storeElementData"></masked-input>
                 </Col>
               </Row>
             </div>
@@ -78,9 +76,7 @@
             <div class="adm-form__item_content">
               <Row :gutter="16" type="flex" align="middle">
                 <Col :xs="24" :md="14" :lg="16">
-                  <masked-input inputClass="adm-input adm-input--regular wmax240 wmin180" v-model="data.color"
-                                :maskProps="{regex: '[a-zA-Zа-яА-Я0-9]+', placeholder: '', casing: 'upper'}"
-                                @onInputChange="storeElementData"></masked-input>
+                  <masked-input inputClass="adm-input adm-input--regular wmax240 wmin180" v-model="data.color" :maskProps="{regex: '[a-zA-Zа-яА-Я0-9 ]+', placeholder: '', casing: 'upper'}" @onInputChange="storeElementData"></masked-input>
                 </Col>
               </Row>
             </div>
--- src/components/wizard/items/protEvac/WizardItemVehsEvac.vue
+++ src/components/wizard/items/protEvac/WizardItemVehsEvac.vue
@@ -78,9 +78,7 @@
             <div class="adm-form__item_content">
               <Row :gutter="16" type="flex" align="middle">
                 <Col :xs="24" :md="14" :lg="16">
-                  <masked-input inputClass="adm-input adm-input--regular wmax240 wmin180" v-model="data.color"
-                                :maskProps="{regex: '[a-zA-Zа-яА-Я0-9]+', placeholder: '', casing: 'upper'}"
-                                @onInputChange="storeElementData"></masked-input>
+                  <masked-input inputClass="adm-input adm-input--regular wmax240 wmin180" v-model="data.color" :maskProps="{regex: '[a-zA-Zа-яА-Я0-9 ]+', placeholder: '', casing: 'upper'}" @onInputChange="storeElementData"></masked-input>
                 </Col>
               </Row>
             </div>
