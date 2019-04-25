29e4419d30e463b36297554577471dc44f5f53ba
Semin Dmitry <semin@imp-m.ru>
2019-04-25 16:15:00 +0300

    мелкие правки

--- src/components/delo/edit/FrmEdDocsOpredEdit.vue
+++ src/components/delo/edit/FrmEdDocsOpredEdit.vue
@@ -148,6 +148,129 @@
                   </Row>
                 </div>
               </div>
+              <div class="adm-form__item">
+                <small class="adm-form__label">Справочник типов источников</small>
+                <Row :gutter="16" type="flex" align="middle">
+                  <Col>
+                    <Select class="wmax240 wmin180 adm-input adm-input--regular" placeholder="" v-model="docsOpred.vsTypeId" clearable @on-change="store">
+                      <Option class="wmax360 " v-for="item in violSourceTypeList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                    </Select>
+                  </Col>
+                </Row>
+              </div>
+              <div v-if="docsOpred.vsTypeId === 1" class="adm-form__item">
+                <small class="adm-form__label">Фомилия</small>
+                <Row :gutter="16" type="flex" align="middle">
+                  <Col>
+                    <masked-input inputClass="adm-input adm-input--regular wmax240 wmin180" v-model="docsOpred.vsFlFirstName"
+                                  :maskProps="{casing: 'upper', regex: '[a-zA-Zа-яА-Я]+', placeholder: ''}"
+                                  @onInputChange="store"></masked-input>
+                  </Col>
+                </Row>
+              </div>
+              <div v-if="docsOpred.vsTypeId === 1" class="adm-form__item">
+                <small class="adm-form__label">Имя</small>
+                <Row :gutter="16" type="flex" align="middle">
+                  <Col>
+                    <masked-input inputClass="adm-input adm-input--regular wmax240 wmin180" v-model="docsOpred.vsFlSecondName"
+                                  :maskProps="{casing: 'upper', regex: '[a-zA-Zа-яА-Я]+', placeholder: ''}"
+                                  @onInputChange="store"></masked-input>
+                  </Col>
+                </Row>
+              </div>
+              <div v-if="docsOpred.vsTypeId === 1" class="adm-form__item">
+                <small class="adm-form__label">Отчество</small>
+                <Row :gutter="16" type="flex" align="middle">
+                  <Col>
+                    <masked-input inputClass="adm-input adm-input--regular wmax240 wmin180" v-model="docsOpred.vsFlThirdName"
+                                  :maskProps="{casing: 'upper', regex: '[a-zA-Zа-яА-Я]+', placeholder: ''}"
+                                  @onInputChange="store"></masked-input>
+                  </Col>
+                </Row>
+              </div>
+              <div v-if="docsOpred.vsTypeId === 1" class="adm-form__item">
+                <small class="adm-form__label">Пол</small>
+                <div class="adm-form__item_content">
+                  <Row :gutter="16" type="flex" align="middle">
+                    <Col :xs="24" :md="24" :lg="24">
+                      <Select class="adm-input adm-input--regular wmin180 wmax240" placeholder="" v-model="docsOpred.vsFlSex" clearable filterable @on-change="store">
+                        <Option class="" v-for="item in sexList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                      </Select>
+                    </Col>
+                  </Row>
+                </div>
+              </div>
+
+              <div v-if="docsOpred.vsTypeId !== 1" class="adm-form__item">
+                <small class="adm-form__label">Наименование источника данных о нарушении</small>
+                <div class="adm-form__item_content">
+                  <Row :gutter="16" type="flex" align="middle">
+                    <Col :xs="24" :md="24" :lg="24">
+                      <Input class="adm-input adm-input--regular" @on-input-change="store" v-model="docsOpred.vsName" ></Input>
+                    </Col>
+                  </Row>
+                </div>
+              </div>
+              <div class="adm-form__item">
+                <small class="adm-form__label">Вхоящий номер документа, полученного от источника данных о нарушении</small>
+                <div class="adm-form__item_content">
+                  <Row :gutter="16" type="flex" align="middle">
+                    <Col :xs="24" :md="24" :lg="24">
+                      <Input class="adm-input adm-input--regular" @on-input-change="store" v-model="docsOpred.vsIncomingNumber" ></Input>
+                    </Col>
+                  </Row>
+                </div>
+              </div>
+              <div class="adm-form__item">
+                <small class="adm-form__label">Входящая дата документа, полученного от источника данных о нарушении</small>
+                <div class="adm-form__item_content">
+                  <Row :gutter="16" type="flex" align="middle">
+                    <Col :xs="24" :md="24" :lg="24">
+                      <DatePickerMask class="adm-input adm-input--regular wmin120 wmax180" v-model="docsOpred.vsIncomingDate" @change="store" clearable type="date" placeholder="дд/мм/гггг" momentFormat="DD/MM/YYYY" maskFormat="dd/mm/yyyy"></DatePickerMask>
+                    </Col>
+                  </Row>
+                </div>
+              </div>
+              <div v-if="docsOpred.vsTypeId !== 1" class="adm-form__item">
+                <small class="adm-form__label">Номер разрешения такси</small>
+                <div class="adm-form__item_content">
+                  <Row :gutter="16" type="flex" align="middle">
+                    <Col :xs="24" :md="24" :lg="24">
+                      <Input class="adm-input adm-input--regular" @on-input-change="store" v-model="docsOpred.tlNumber" ></Input>
+                    </Col>
+                  </Row>
+                </div>
+              </div>
+              <div class="adm-form__item">
+                <small class="adm-form__label">Дата начала срока действия разрешения такси</small>
+                <div class="adm-form__item_content">
+                  <Row :gutter="16" type="flex" align="middle">
+                    <Col :xs="24" :md="24" :lg="24">
+                      <DatePickerMask class="adm-input adm-input--regular wmin120 wmax180" v-model="docsOpred.tlDateBeg" @change="store" clearable type="date" placeholder="дд/мм/гггг" momentFormat="DD/MM/YYYY" maskFormat="dd/mm/yyyy"></DatePickerMask>
+                    </Col>
+                  </Row>
+                </div>
+              </div>
+              <div class="adm-form__item">
+                <small class="adm-form__label">Дата окончания срока действия разрешения такси</small>
+                <div class="adm-form__item_content">
+                  <Row :gutter="16" type="flex" align="middle">
+                    <Col :xs="24" :md="24" :lg="24">
+                      <DatePickerMask class="adm-input adm-input--regular wmin120 wmax180" v-model="docsOpred.tlDateEnd" @change="store" clearable type="date" placeholder="дд/мм/гггг" momentFormat="DD/MM/YYYY" maskFormat="dd/mm/yyyy"></DatePickerMask>
+                    </Col>
+                  </Row>
+                </div>
+              </div>
+              <div class="adm-form__item">
+                <small class="adm-form__label">Организация, выдавшая разрешение такси</small>
+                <div class="adm-form__item_content">
+                  <Row :gutter="16" type="flex" align="middle">
+                    <Col :xs="24" :md="24" :lg="24">
+                      <Input class="adm-input adm-input--regular" @on-input-change="store" v-model="docsOpred.tlLicensor" ></Input>
+                    </Col>
+                  </Row>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -196,6 +319,7 @@
           alert(error);
         } else {
           await this.fillPnpaList();
+          await this.fillViolSourceTypeList();
 
           this.docsOpred = docsOpred;
 
@@ -463,6 +587,17 @@
               }
             ]
         },
+        violSourceTypeList: null,
+        sexList: [
+          {
+            label: 'Мужской',
+            value: 'М'
+          },
+          {
+            label: 'Женский',
+            value: 'Ж'
+          },
+        ],
       }
     },
     methods: {
@@ -535,6 +670,21 @@
           });
         }
         this.stotvSearchInfoList = stotvSearchInfoList;
+      },
+      async fillViolSourceTypeList() {
+        let eventResponse = await RequestApi.prepareData({
+          method: 'getViolSourceTypeDict'
+        });
+        let violSourceTypeList = [];
+        let violSourceTypeDict = JSON.parse(eventResponse.response).data;
+        for (let i = 0; i < violSourceTypeDict.length; i++) {
+          let violSourceType = violSourceTypeDict[i];
+          violSourceTypeList.push({
+            label: violSourceType.NAME,
+            value: violSourceType.ID
+          });
+        }
+        this.violSourceTypeList = violSourceTypeList;
       },
 
       async changeInspSostKod() {
