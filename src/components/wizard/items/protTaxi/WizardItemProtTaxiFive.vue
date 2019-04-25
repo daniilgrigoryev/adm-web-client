8980da8f227ee16460c665f009020861b1af860f
Semin Dmitry <semin@imp-m.ru>
2019-04-25 16:31:51 +0300

    мелкие правки

--- src/components/wizard/items/protTaxi/WizardItemProtTaxiFive.vue
+++ src/components/wizard/items/protTaxi/WizardItemProtTaxiFive.vue
@@ -28,44 +28,6 @@
         </Row>
       </div>
     </div>
-
-    <div class="adm-form__item">
-      <small class="adm-form__label">Номер разрешения</small>
-      <Row :gutter="16" type="flex" align="middle">
-        <Col :xs="24" :md="14" :lg="16">
-          <masked-input inputClass="adm-input adm-input--regular" v-model="data.licenseNum" :maskProps="{casing: 'upper', regex: '[0-9]+', placeholder: ''}" @onInputChange="storeElementData" ></masked-input>
-        </Col>
-      </Row>
-    </div>
-
-    <div class="adm-form__item">
-      <small class="adm-form__label">Дата начала действия</small>
-      <Row :gutter="16" type="flex" align="middle">
-        <Col :xs="24" :md="14" :lg="16">
-          <DatePickerMask class="adm-input adm-input--regular wmin120 wmax180" v-model="data.startDate" @change="storeElementData" clearable type="date" placeholder="дд/мм/гггг" momentFormat="DD/MM/YYYY" maskFormat="dd/mm/yyyy"></DatePickerMask>
-        </Col>
-      </Row>
-    </div>
-
-    <div class="adm-form__item">
-      <small class="adm-form__label">Дата окончания действия</small>
-      <Row :gutter="16" type="flex" align="middle">
-        <Col :xs="24" :md="14" :lg="16">
-          <DatePickerMask class="adm-input adm-input--regular wmin120 wmax180" v-model="data.stopDate" @change="storeElementData" clearable type="date" placeholder="дд/мм/гггг" momentFormat="DD/MM/YYYY" maskFormat="dd/mm/yyyy"></DatePickerMask>
-        </Col>
-      </Row>
-    </div>
-
-    <div class="adm-form__item">
-      <small class="adm-form__label">Статус держателя лицензии</small>
-      <Row :gutter="16" type="flex" align="middle">
-        <Col :xs="24" :md="14" :lg="16">
-          <Select class="adm-input adm-input--regular wmin180" ref="city" placeholder="" v-model="data.status" filterable clearable @on-change="storeElementData">
-            <Option class="" v-for="item in statusList" :value="item.value" :key="item.value">{{ item.label }}</Option>
-          </Select>
-        </Col>
-      </Row>
-    </div>
   </div>
 </template>
 
@@ -90,8 +52,7 @@
     },
     data() {
       return {
-        data: null,
-        statusList: null
+        data: null
       }
     },
     methods: {
@@ -108,30 +69,9 @@
           alert(error);
         } else {
           this.data = data;
-          this.fillStatusList();
         }
       },
 
-      async fillStatusList() {
-        let eventResponse = await RequestApi.prepareData({
-          method: 'invokeElementMethod',
-          params: {
-            eCID: this.info.eCID,
-            methodName: 'getStatusDict',
-            data: null
-          }
-        });
-        let statusList = [];
-        let statusDict = JSON.parse(JSON.parse(eventResponse.response).data);
-        for (let i = 0; i < statusDict.length; i++) {
-          let status = statusDict[i];
-          statusList.push({
-            label: status.UCHAST_STATUS_NAME,
-            value: status.UCHAST_STATUS
-          });
-        }
-        this.statusList = statusList;
-      },
       storeElementData() {
         this.$emit('storeElementData', {
           eCID: this.info.eCID,
---
+++ src/components/wizard/items/protTaxi/WizardItemProtTaxiFour.vue
@@ -0,0 +1,181 @@
+<template>
+  <div v-if="data">
+    <div class="adm-form__item">
+      <small class="adm-form__label">Пункт НПА</small>
+      <div class="adm-form__item_content">
+        <Row :gutter="16" type="flex" align="middle">
+          <Col :xs="24" :md="22" :lg="22">
+            <Select class="adm-input adm-input--regular wmin180" placeholder="" v-model="data.pnpaId" clearable filterable @on-change="storeElementData">
+              <Option class="" v-for="item in pnpaList" :value="item.id" :key="item.id">{{ item.value + ', ' + item.label }}</Option>
+            </Select>
+          </Col>
+        </Row>
+      </div>
+    </div>
+    <div class="adm-form__item">
+      <small class="adm-form__label">Статья КРФоАП</small>
+      <div class="adm-form__item_content">
+        <Row :gutter="16" type="flex" align="middle">
+          <Col :xs="24" :md="22" :lg="22">
+            <Select class="adm-input adm-input--regular wmin180" placeholder="" v-model="data.stotvId" clearable filterable @on-change="changeStotvSearchInfo">
+              <Option class="" v-for="item in stotvSearchInfoList" :value="item.id" :key="item.id">{{ item.value + ', ' + item.label }}</Option>
+            </Select>
+          </Col>
+        </Row>
+      </div>
+    </div>
+    <div class="adm-form__item" v-if="KBKSearchInfoList">
+      <small class="adm-form__label">КБК</small>
+      <Row :gutter="16" type="flex" align="middle">
+        <Col :xs="24" :md="14" :lg="16">
+          <Select class="adm-input adm-input--regular wmax240 wmin180" placeholder="" v-model="data.kbk" clearable filterable :disabled="!data.stotvId" @on-change="storeElementData">
+            <Option class="wmax360 " v-for="item in KBKSearchInfoList" :value="item.id" :key="item.id">{{ item.value + ', ' + item.label }}</Option>
+          </Select>
+        </Col>
+      </Row>
+    </div>
+    <div class="adm-form__item">
+      <small class="adm-form__label">Фактические сведения</small>
+      <div class="adm-form__item_content">
+        <Row :gutter="16" type="flex" align="middle">
+          <Col :xs="24" :md="22" :lg="22">
+            <Input class="adm-input adm-input--regular" @on-input-change="storeElementData" v-model="data.factSved" ></Input>
+          </Col>
+        </Row>
+      </div>
+    </div>
+    <div class="adm-form__item">
+      <small class="adm-form__label">Дополнительные сведения</small>
+      <div class="adm-form__item_content">
+        <Row :gutter="16" type="flex" align="middle">
+          <Col :xs="24" :md="22" :lg="22">
+            <Input class="adm-input adm-input--regular" @on-input-change="storeElementData" v-model="data.dopSved" ></Input>
+          </Col>
+        </Row>
+      </div>
+    </div>
+
+
+    <div class="adm-form__item">
+      <small class="adm-form__label">Номер разрешения</small>
+      <Row :gutter="16" type="flex" align="middle">
+        <Col :xs="24" :md="14" :lg="16">
+          <masked-input inputClass="adm-input adm-input--regular" v-model="data.licenseNum" :maskProps="{casing: 'upper', regex: '[0-9]+', placeholder: ''}" @onInputChange="storeElementData" ></masked-input>
+        </Col>
+      </Row>
+    </div>
+
+    <div class="adm-form__item">
+      <small class="adm-form__label">Дата начала действия</small>
+      <Row :gutter="16" type="flex" align="middle">
+        <Col :xs="24" :md="14" :lg="16">
+          <DatePickerMask class="adm-input adm-input--regular wmin120 wmax180" v-model="data.startDate" @change="storeElementData" clearable type="date" placeholder="дд/мм/гггг" momentFormat="DD/MM/YYYY" maskFormat="dd/mm/yyyy"></DatePickerMask>
+        </Col>
+      </Row>
+    </div>
+
+    <div class="adm-form__item">
+      <small class="adm-form__label">Дата окончания действия</small>
+      <Row :gutter="16" type="flex" align="middle">
+        <Col :xs="24" :md="14" :lg="16">
+          <DatePickerMask class="adm-input adm-input--regular wmin120 wmax180" v-model="data.stopDate" @change="storeElementData" clearable type="date" placeholder="дд/мм/гггг" momentFormat="DD/MM/YYYY" maskFormat="dd/mm/yyyy"></DatePickerMask>
+        </Col>
+      </Row>
+    </div>
+
+    <div class="adm-form__item">
+      <small class="adm-form__label">Статус держателя лицензии</small>
+      <Row :gutter="16" type="flex" align="middle">
+        <Col :xs="24" :md="14" :lg="16">
+          <Select class="adm-input adm-input--regular wmin180" ref="city" placeholder="" v-model="data.status" filterable clearable @on-change="storeElementData">
+            <Option class="" v-for="item in statusList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+          </Select>
+        </Col>
+      </Row>
+    </div>
+  </div>
+</template>
+
+<script>
+  import * as funcUtils from "~/assets/js/utils/funcUtils";
+  import RequestApi from "~/assets/js/api/requestApi";
+  import wizardItemProtFourMethods from "~/components/mixins/prot/wizardItemProtFourMethods";
+
+  export default {
+    name: "WizardItemProtFour",
+    mixins: [wizardItemProtFourMethods],
+    props: {
+      info: Object
+    },
+    components: {
+      MaskedInput: () => import('~/components/shared/MaskedInput'),
+      DatePickerMask: () => import('~/components/shared/dateTimePicker/DatePickerMask')
+    },
+    async created() {
+      await this.initData();
+    },
+    data() {
+      return {
+        data: null,
+        statusList: null,
+      }
+    },
+    methods: {
+      async initData() {
+        let eventResponse = await RequestApi.prepareData({
+          method: 'getElementData',
+          params: {
+            eCID: this.info.eCID
+          }
+        });
+        let data = JSON.parse(JSON.parse(eventResponse.response).data);
+        if (funcUtils.isEmpty(data)) {
+          let error = JSON.parse(eventResponse.response).error.errorMsg;
+          alert(error);
+        } else {
+          await this.fillPnpaList();
+          await this.fillStotvSearchInfo();
+          await this.fillStatusList();
+
+          this.data = data;
+        }
+
+        if (funcUtils.isNotEmpty(this.data.stotvId)) {
+          this.fillKBKSearchInfo();
+        }
+      },
+
+      async fillStatusList() {
+        let eventResponse = await RequestApi.prepareData({
+          method: 'invokeElementMethod',
+          params: {
+            eCID: this.info.eCID,
+            methodName: 'getStatusDict',
+            data: null
+          }
+        });
+        let statusList = [];
+        let statusDict = JSON.parse(JSON.parse(eventResponse.response).data);
+        for (let i = 0; i < statusDict.length; i++) {
+          let status = statusDict[i];
+          statusList.push({
+            label: status.UCHAST_STATUS_NAME,
+            value: status.UCHAST_STATUS
+          });
+        }
+        this.statusList = statusList;
+      },
+
+      storeElementData() {
+        this.$emit('storeElementData', {
+          eCID: this.info.eCID,
+          data: this.data
+        });
+      },
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
