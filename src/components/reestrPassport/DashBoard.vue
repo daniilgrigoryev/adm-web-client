474d3f116b4d01695c732dbbf23b9952b0717f94
Grachev <grachev@stdpr.ru>
2019-04-29 18:08:15 +0300

    new dashboard

--- src/components/reestrPassport/DashBoard.vue
+++ src/components/reestrPassport/DashBoard.vue
@@ -1,257 +1,64 @@
 <template>
-    <div>
-      <div class="wmax1920 mx-auto">
-        <!-- <a href="#" @click="getPrev()">Назад</a> -->
-
-				<div class="flex-parent flex-parent--center-main flex-parent--center-cross" style="margin-top: 6%"><!-- style="height: calc(100vh - 66px);" -->
-					<div>
-						<Row type="flex" justify="center" :gutter="60">
-							<Col>
-								<Card class="truck-card px24 mb60" style="min-width: 550px">
-									<p class="adm-txt-regular color-dark-light my18">Эвакуация</p>
-									<ul>
-										<li class="my18">
-											<a href="#" @click="createWizardScenarioPZTC" class="adm-text-big link color-blue-base txt-underline-on-hover">Протокол о задержании транспортного средства</a>
-										</li>
-										<li class="my18">
-											<a href="#" @click="createWizardScenarioAPN" class="adm-text-big link color-blue-base txt-underline-on-hover">Протокол об административном правонарушении</a>
-										</li>
-										<li class="my18">
-											<a href="#" @click="createWizardScenarioDefinition" class="adm-text-big link color-blue-base txt-underline-on-hover">Определение о возбуждении дела об <br> административном правонарушении</a>
-										</li>
-									</ul>
-								</Card>
-								<Card class="px24 mb60 py12 mt60" style="min-width: 550px">
-									<div class="flex-parent flex-parent--center-cross">
-										<div class="mr36">
-											<img src="../../assets/images/case.svg" alt="">
-										</div>
-										<a href="#" @click="createWizardScenarioPost" class="adm-text-big link color-blue-base txt-underline-on-hover">Постановление об  административном <br> правонарушении</a>
-									</div>
-								</Card>
-								<!-- <Row type="flex" justify="center">
-									<Col>
-										<Card class="px24 mr24" style="min-width: 255px">
-											<div class="flex-parent flex-parent--column flex-parent--center-cross">
-												<div class="mb12">
-													<img src="../../assets/images/camera.png" alt="">
-												</div>
-												<a href="" class="adm-text-big link color-blue-base txt-underline-on-hover">Фотофиксация нарушения</a>
-											</div>
-										</Card>
-									</Col>
-									<Col>
-										<Card class="px24 ml24" style="min-width: 255px">
-											<div class="flex-parent flex-parent--column flex-parent--center-cross">
-												<div class="mb12">
-													<img src="../../assets/images/tax.png" alt="">
-												</div>
-												<a href="#" @click="createWizardScenario2025" class="adm-text-big link color-blue-base txt-underline-on-hover">Неуплата штрафа</a>
-											</div>
-										</Card>
-									</Col>
-								</Row> -->
-							</Col>
-							<Col>
-								<Card class="taxi-card px24 mb60" style="min-width: 550px">
-									<p class="adm-txt-regular color-dark-light my18">Такси</p>
-									<ul>
-										<li class="my18">
-											<a href="#" @click="createWizardScenarioProtTaxi" class="adm-text-big link color-blue-base txt-underline-on-hover">Протокол об административном правонарушении</a>
-										</li>
-										<li class="my18">
-											<a href="#" @click="createWizardScenarioProtIzyat" class="adm-text-big link color-blue-base txt-underline-on-hover">Протокол об изъятии вещей и документов</a>
-										</li>
-										<li class="my18">
-											<a href="#" @click="createWizardScenarioDefinitionTaxi" class="adm-text-big link color-blue-base txt-underline-on-hover">Определение о возбуждении дела об <br/> административном правонарушении</a>
-										</li>
-									</ul>
-								</Card>
-								<Card class="px24 py12 mt60" style="min-width: 550px">
-									<div class="flex-parent flex-parent--center-cross">
-										<div class="mr36 h60 w60">
-											<img src="../../assets/images/tax.svg" class="h-full" alt="">
-										</div>
-										<a href="#" @click="createWizardScenarioPost" class="adm-text-big link color-blue-base txt-underline-on-hover">Возбуждение дела об административном <br> правонарушении по статье 20.25</a>
-									</div>
-								</Card>
-
-								<!-- <Card class="px24 ml60" style="min-width: 320px">
-									<div class="flex-parent flex-parent--column flex-parent--center-cross">
-										<div class="mb12">
-											<img src="../../assets/images/police.png" alt="">
-										</div>
-										<a href="" class="adm-text-big link color-blue-base txt-underline-on-hover">Служба судебных приставов</a>
-									</div>
-								</Card> -->
-							</Col>
-						</Row>
-					</div>
-				</div>
-
-      </div>
+  <div>
+    <div class="buttons-wrap">
+      <label>
+        <input type="radio" name="da" value="DashBoardOld" v-model="picked" />
+        <div class="radio-text">Old</div>
+      </label>
+      <label>
+        <input type="radio" name="da" value="DashBoardNew" v-model="picked" />
+        <div class="radio-text">New</div>
+      </label>
     </div>
+    <component :is="picked"></component>
+  </div>
 </template>
 
 <script>
-  import * as formStack from '~/assets/js/api/formStack';
+import DashBoardOld from "./DashBoard_old.vue";
+import DashBoardNew from "./DashBoard_new.vue";
 
-  export default {
-    name: "DashBoard",
-    data() {
-		  return {}
-    },
-    methods: {
-      createWizardScenarioPZTC() {
-        try {
-          let params = {
-            scenarioName: 'CreateProtEvac'
-          };
-
-          formStack.toNext({
-            module: this.$store.state.wizardExecuter,
-            vm: this,
-            notRemoved: true,
-            params: params,
-            withCreate: true
-          });
-        } catch (e) {
-          alert(e.message);
-        }
-      },
-      createWizardScenarioAPN() {
-        try {
-          let params = {
-            scenarioName: 'CreateProtAPN'
-          };
-
-          formStack.toNext({
-            module: this.$store.state.wizardExecuter,
-            vm: this,
-            notRemoved: true,
-            params: params,
-            withCreate: true
-          });
-        } catch (e) {
-          alert(e.message);
-        }
-      },
-      createWizardScenarioProtTaxi() {
-        try {
-          let params = {
-            scenarioName: 'CreateProtTaxi'
-          };
-
-          formStack.toNext({
-            module: this.$store.state.wizardExecuter,
-            vm: this,
-            notRemoved: true,
-            params: params,
-            withCreate: true
-          });
-        } catch (e) {
-          alert(e.message);
-        }
-      },
-      createWizardScenarioPost() {
-        try {
-          let params = {
-            scenarioName: 'CreatePost'
-          };
-
-          formStack.toNext({
-            module: this.$store.state.wizardExecuter,
-            vm: this,
-            notRemoved: true,
-            params: params,
-            withCreate: true
-          });
-        } catch (e) {
-          alert(e.message);
-        }
-      },
-      createWizardScenarioProtIzyat() {
-        try {
-          let params = {
-            scenarioName: 'CreateProtIzyat'
-          };
-
-          formStack.toNext({
-            module: this.$store.state.wizardExecuter,
-            vm: this,
-            notRemoved: true,
-            params: params,
-            withCreate: true
-          });
-        } catch (e) {
-          alert(e.message);
-        }
-      },
-      createWizardScenarioDefinition() {
-        try {
-          let params = {
-            scenarioName: 'CreateDefinition'
-          };
-
-          formStack.toNext({
-            module: this.$store.state.wizardExecuter,
-            vm: this,
-            notRemoved: true,
-            params: params,
-            withCreate: true
-          });
-        } catch (e) {
-          alert(e.message);
-        }
-      },
-      createWizardScenarioDefinitionTaxi() {
-        try {
-          let params = {
-            scenarioName: 'CreateDefinitionTaxi'
-          };
-
-          formStack.toNext({
-            module: this.$store.state.wizardExecuter,
-            vm: this,
-            notRemoved: true,
-            params: params,
-            withCreate: true
-          });
-        } catch (e) {
-          alert(e.message);
-        }
-      },
-      createWizardScenario2025() {
-        try {
-          let params = {
-            scenarioName: 'CreateProt2025'
-          };
-
-          formStack.toNext({
-            module: this.$store.state.wizardExecuter,
-            vm: this,
-            notRemoved: true,
-            params: params,
-            withCreate: true
-          });
-        } catch (e) {
-          alert(e.message);
-        }
-      },
-    }
+export default {
+  components: {
+    DashBoardOld,
+    DashBoardNew
+  },
+  data() {
+    return {
+      picked: DashBoardOld
+    };
   }
+};
 </script>
 
 <style lang="scss" scoped>
-	.truck-card{
-		background-image: url('../../assets/images/truck.png');
-		background-repeat: no-repeat;
-		background-position:  97% 97%;
-	}
-	.taxi-card{
-		background-image: url('../../assets/images/taxi.png');
-		background-repeat: no-repeat;
-		background-position:  97% 97%;
-	}
-</style>
+.buttons-wrap {
+  display: grid;
+  grid-gap: 10px;
+  grid-auto-flow: column;
+  justify-content: center;
+  padding: 10px 0 0;
+  label {
+    cursor: pointer;
+    input {
+      display: none;
+      &:checked {
+        & ~ .radio-text {
+          background: #1888CC;
+        }
+      }
+    }
+    .radio-text {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 80px;
+      color: #ffffff;
+      background: rgb(160, 160, 160);
+      height: 30px;
+      border-radius: 5px;
 
+    }
+  }
+}
+</style>
---
+++ src/components/reestrPassport/DashBoard_new.vue
@@ -0,0 +1,256 @@
+<template>
+  <section class="dashboard">
+    <div class="dashboard__breadcrumbs">
+      <Button
+        v-if="breadcrumbs.length > 1"
+        class="prev"
+        @click="toPrevListItem()"
+        type="text"
+        title="вернуться назад"
+      >
+        <Icon type="ios-arrow-dropleft" :size="30" />
+      </Button>
+      <ul>
+        <li
+          v-for="(item, index) in breadcrumbs"
+          :key="item.id"
+          @click="toListItem(item, index)"
+        >
+          {{ item.title }}
+        </li>
+      </ul>
+    </div>
+    <ul class="dashboard__list">
+      <li v-for="item in currentList" :key="item.id" @click="openItem(item)">
+        <div :class="{ gridWrap: item.icon }">
+          <div v-if="item.icon" class="img-wrap">
+            <img :src="item.icon" alt="" />
+          </div>
+          <div
+            v-if="item.title"
+            class="title"
+            :class="{ tdu: !Array.isArray(item.value) }"
+          >
+            {{ item.title }}
+          </div>
+          <div v-if="item.desc" class="desc">{{ item.desc }}</div>
+        </div>
+        <Button v-if="Array.isArray(item.value)" type="text">
+          <Icon type="ios-arrow-dropright" :size="30" />
+        </Button>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import * as formStack from "~/assets/js/api/formStack";
+
+export default {
+  data() {
+    return {
+      defoultList: [
+        {
+          title: "Определение",
+          desc: "о возбуждении дела об административном расследовании",
+          icon: require("../../assets/images/grayIcons/Документ_4 gray.png"),
+          value: "CreateDefinition"
+        },
+        {
+          title: "Протоколы",
+          desc: "Общая форма, такси, эвакуация",
+          icon: require("../../assets/images/grayIcons/Документ gray.png"),
+          value: [
+            {
+              title: "Об административном правонарушении",
+              value: [
+                {
+                  title: "Общее",
+                  value: "CreateProtAPN"
+                },
+                {
+                  title: "Такси",
+                  value: "CreateProtTaxi"
+                },
+                {
+                  title: "Эвакуация",
+                  value: ""
+                },
+                {
+                  title: "КоАП г. Москвы",
+                  desc: "статья 8.25. Размещение транспортных средств на территории, занятой зелеными насаждениями) по 20.25(Уклонение от исполнения административного наказания",
+                  value: "",
+                },
+              ]
+            },
+            {
+              title: "О задержании ТС",
+              value: [
+                {
+                  title: "Эвакуация",
+                  value: "CreateProtEvac"
+                }
+              ]
+            },
+            {
+              title: "Об изъятии вещей и документов",
+              value: [
+                {
+                  title: "Такси",
+                  value: "CreateProtIzyat"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          title: "Постановление",
+          desc: "об административном правонарушении",
+          icon: require("../../assets/images/grayIcons/10_Постановление_v1 gray.png"),
+          value: "CreatePost"
+        }
+      ],
+      currentList: [],
+      breadcrumbs: []
+    };
+  },
+  created() {
+    this.currentList = this.defoultList;
+    this.breadcrumbs.push({
+      title: "Возбуждение дела",
+      value: this.defoultList
+    });
+  },
+  methods: {
+    openItem(item) {
+      if (!item.value.length) {
+        return;
+      }
+      if (Array.isArray(item.value)) {
+        this.currentList = item.value;
+        this.breadcrumbs.push(item);
+      } else {
+        formStack.toNext({
+          module: this.$store.state.wizardExecuter,
+          vm: this,
+          notRemoved: true,
+          params: {scenarioName: item.value},
+          withCreate: true
+        });
+      }
+    },
+    toListItem(item, index) {
+      this.breadcrumbs.splice(index + 1);
+      this.currentList = item.value;
+    },
+    toPrevListItem() {
+      this.breadcrumbs.splice(this.breadcrumbs.length - 1);
+      this.currentList = this.breadcrumbs[this.breadcrumbs.length - 1].value;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  margin: 100px auto;
+  background: #fff;
+  width: 729px;
+  border-radius: 4px;
+  border: 1px solid #ececec;
+  .dashboard__breadcrumbs {
+    border-bottom: 1px solid #ececec;
+    display: flex;
+    align-items: center;
+    height: 56px;
+    padding: 0 30px;
+    ul {
+      display: flex;
+      align-items: center;
+      font-size: 18px;
+      li {
+        padding: 0 5px;
+        position: relative;
+        cursor: pointer;
+        line-height: 24px;
+        &:hover {
+          text-decoration: underline;
+        }
+        &:after {
+          content: "/";
+          position: absolute;
+          right: -2px;
+          top: 0;
+          bottom: 0;
+          margin: auto;
+        }
+        &:last-child {
+          color: #1888cc;
+          font-weight: 500;
+          pointer-events: none;
+          &:after {
+            content: none;
+          }
+        }
+        &:first-child {
+          color: #8c8c8c;
+        }
+      }
+    }
+  }
+  .dashboard__list {
+    li {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      border-bottom: 1px solid #ececec;
+      cursor: pointer;
+      padding: 20px;
+      padding-left: 36px;
+      .title {
+        color: #1888cc;
+        font-size: 20px;
+        font-weight: 600;
+        &.tdu {
+          text-decoration: underline;
+          &:hover {
+            text-decoration: none;
+          }
+        }
+      }
+      .gridWrap {
+        display: grid;
+        grid-template-columns: 56px 1fr;
+        grid-gap: 5px 30px;
+        .img-wrap {
+          width: 56px;
+          height: 56px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          grid-row: span 2;
+          opacity: 0.6;
+        }
+        .title {
+          grid-column: 2;
+        }
+        .desc {
+          color: #c6c6c6;
+          font-size: 16px;
+          line-height: 18px;
+        }
+      }
+      button {
+        color: #57a3f3;
+      }
+    }
+  }
+}
+</style>
+
+<style lang="scss">
+.dashboard .ivu-btn {
+  padding: 0;
+}
+</style>
+
