77174cb5716d0fe4c3aeecef982cc9b9e83041d5
Daniil Grigoryev <grigorev@stdpr.ru>
2019-04-30 17:50:57 +0300

    Улучшение #24244

--- src/components/delo/edit/FrmEdDecisEdit.vue
+++ src/components/delo/edit/FrmEdDecisEdit.vue
@@ -210,7 +210,7 @@
                   <div class="adm-form__item_content">
                     <Row :gutter="16" type="flex" align="middle">
                       <Col :xs="24" :md="24" :lg="24">
-                        <DatePickerMask class="adm-input adm-input--regular wmax240" v-model="decis.dateRasm" @change="store" clearable type="date" placeholder="дд/мм/гггг" momentFormat="DD/MM/YYYY" maskFormat="dd/mm/yyyy"></DatePickerMask>
+                        <DatePickerMask class="adm-input adm-input--regular wmax240" v-model="decis.dateRasm" @change="store" clearable type="datetime"  placeholder="дд/мм/гггг чч:мм" momentFormat="DD/MM/YYYY HH:mm" maskFormat="dd/mm/yyyy HH:MM"></DatePickerMask>
                       </Col>
                     </Row>
                   </div>
@@ -227,7 +227,7 @@
                   </Row>
                 </div>
               </div>
-              <div class="adm-form__item">
+              <div v-if="!decis.decisKod && showByDecisKod(!decisKods.rasm)" class="adm-form__item" >
                 <small class="adm-form__label">Дата вступления</small>
                 <div class="adm-form__item_content">
                   <Row :gutter="16" type="flex" align="middle">
