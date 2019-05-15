ae268f20e5bbab35a955e1b6716c3c19c7db3676
Semin Dmitry <semin@imp-m.ru>
2019-05-15 13:54:54 +0300

    мелкие правки

--- src/components/delo/FrmEdDocsPost.vue
+++ src/components/delo/FrmEdDocsPost.vue
@@ -69,12 +69,59 @@
             style="grid-column: span 2;"
             :icon="require('../../assets/images/map.svg')"
           />
-
-          <ul v-if="docSignatures && false">
-            <li v-for="(itemSign, signIdx) in docSignatures" :key="signIdx">
-              {{ itemSign.sign }}
-            </li>
-          </ul>
+        </div>
+      </div>
+    </div>
+    <div v-if="docSignatures" class="errors-table errors-table--v2">
+      <div class="errors-table__container">
+        <h2  @click="hideMore = !hideMore" class="adm-form__headding bg-white cursor-pointer flex-parent flex-parent--space-between-main">
+          <span class="adm-h4 color-dark-lighter" style="line-height: inherit;">Подписи</span>
+          <Button type="text" class="bg-transparent" style="box-shadow: none;">
+            <Icon v-if="hideMore" type="md-remove" class="color-gray" :size="25" title="свернуть" />
+            <Icon v-else type="md-add" class="color-gray" :size="25" title="развернуть"/>
+          </Button>
+        </h2>
+        <div class="errors-table__content" v-show="hideMore">
+          <table class='adm-table-simple table table--fixed border--0'>
+            <thead>
+            <tr class="bg-white-light">
+              <th class="w180 align-middle">
+                <p class="adm-text-big txt-normal color-dark-lighter">Издатель сертификата ключа подписи</p>
+              </th>
+              <th class="w180 align-middle">
+                <p class="adm-text-big txt-normal color-dark-lighter">Владелец сертификата ключа подписи</p>
+              </th>
+              <th class="w180 align-middle">
+                <p class="adm-text-big txt-normal color-dark-lighter">Пользователь, создавший документ</p>
+              </th>
+              <th class="w180 align-middle">
+                <p class="adm-text-big txt-normal color-dark-lighter">Время создания</p>
+              </th>
+              <th class="w180 align-middle">
+                <p class="adm-text-big txt-normal color-dark-lighter">Список файлов</p>
+              </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(item, itemIdx) in docSignatures" :key="itemIdx">
+              <td class="align-middle">
+                <p class="adm-14 color-dark-base">{{item.sign.certIssuer}}</p>
+              </td>
+              <td class="align-middle">
+                <p class="adm-14 color-dark-base">{{item.sign.certSubject}}</p>
+              </td>
+              <td class="align-middle">
+                <p class="adm-14 color-dark-base">{{item.sign.createIspName}}</p>
+              </td>
+              <td class="align-middle">
+                <p class="adm-14 color-dark-base">{{item.sign.createTime | formatDateTime('DD.MM.YYYY HH:mm')}}</p>
+              </td>
+              <td class="align-middle">
+                <img class="error-image" alt="img" src="../../assets/images/pdf.svg" title="Скачать документ" v-for="(itemMedia, itemMediaIdx) in item.media" :key="itemMediaIdx" @click="downloadMedia(itemMedia.mediaId)" />
+              </td>
+            </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
@@ -82,6 +129,7 @@
 </template>
 
 <script>
+  import * as ConstantUtils from '~/assets/js/utils/constantUtils';
   import * as funcUtils from "~/assets/js/utils/funcUtils";
   import RequestApi from "~/assets/js/api/requestApi";
   import * as formStack from '~/assets/js/api/formStack';
@@ -120,7 +168,8 @@
     },
     data() {
       return {
-        docSignatures: null
+        docSignatures: null,
+        hideMore: false,
       }
     },
     computed: {
@@ -173,6 +222,32 @@
           this.docSignatures = responseData;
         }
       },
+      async downloadMedia(mediaId) {
+        try {
+          let eventResponse = await RequestApi.sendGetMediaFileHttpRequest({
+            url: `${ConstantUtils.HTTP_URL_FILES}/${localStorage.getItem('admSid')}/${mediaId}`
+          });
+          let data = null;
+          if (eventResponse.response) {
+            data = eventResponse.response;
+          }
+          if (data) {
+            let element = document.createElement("a");
+            element.href = window.URL.createObjectURL(data);
+            element.setAttribute("download", `${funcUtils.guid()}.pdf`);
+            element.style.display = "none";
+            document.body.appendChild(element);
+            element.click();
+            document.body.removeChild(element);
+          }
+        } catch (e) {
+          this.$Notice.warning({
+            title: 'Ошибка получения данных',
+            desc: e.message,
+            duration: 10
+          });
+        }
+      },
       getDocsPostEdit() {
         try {
           let currentForm = innerFormStack.getCurrent();
@@ -212,3 +287,37 @@
     }
   }
 </script>
+
+<style lang="scss" scoped>
+  .errors-table{
+    .table td{
+      border-bottom: 0;
+    }
+    margin-top: 30px;
+    margin-bottom: 30px;
+    .error-th{
+      width: 60px;
+    }
+    .adm-table-simple thead tr th{
+      padding: 8px 12px;
+    }
+    .adm-form__headding{
+      height: 40px;
+      line-height: 40px;
+      padding-left: 40px;
+    }
+
+    .error-image {
+      width: 25px;
+      height: 25px;
+      margin: 5px;
+      cursor: pointer;
+    }
+  }
+  .errors-table--v2 .errors-table__container{
+    border: none;
+    border-radius: 4px;
+    box-shadow: 0 0 1px 0 rgba(0,0,0,0.26), 0 0 4px 0 rgba(0,0,0,0.16);
+    background: #fff;
+  }
+</style>
