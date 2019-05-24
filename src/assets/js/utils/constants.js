945ec69fffdc2345eef38ec9a5397c07ea4c637a
Semin Dmitry <semin@imp-m.ru>
2019-05-24 18:41:48 +0300

    мелкие правки

--- src/assets/js/utils/constants.js
+++ src/assets/js/utils/constants.js
@@ -23,22 +23,41 @@ export const STAD_PERFORMED=6;
 /**Стадия дела Прекращено*/
 export const STAD_TERMINATED=7;
 
+/**Тип элемента дерева */
 export const DTP='DTP';
+/**Тип элемента дерева Дело*/
 export const DELO='DELO';
+/**Тип элемента дерева Предъявленный документ*/
 export const VU_PRED='VU_PRED';
+/**Тип элемента дерева Выданный документ*/
 export const VU_VYD='VU_VYD';
+/**Тип элемента дерева Документ постановления*/
 export const DOCS_POST='DOCS_POST';
+/**Тип элемента дерева */
 export const DOCS_POST_UL='DOCS_POST_UL';
+/**Тип элемента дерева Документ Протокол*/
 export const DOCS_PROT='DOCS_PROT';
+/**Тип элемента дерева */
 export const DOCS_OTHER='DOCS_OTHER';
+/**Тип элемента дерева */
 export const DOCS_TREB='DOCS_TREB';
+/**Тип элемента дерева Документ жалоба*/
 export const DOCS_GALOB='DOCS_GALOB';
+/**Тип элемента дерева Документ определение*/
 export const DOCS_OPRED='DOCS_OPRED';
+/**Тип элемента дерева Участник физ. лицо*/
 export const UCHASTFL='UCHASTFL';
+/**Тип элемента дерева Участник юр. лицо*/
 export const UCHASTUL='UCHASTUL';
+/**Тип элемента дерева */
 export const UCHASTOTHER='UCHASTOTHER';
+/**Тип элемента дерева Транспортное средство*/
 export const VEHS='VEHS';
+/**Тип элемента дерева */
 export const VEHSOTHER='VEHSOTHER';
+/**Тип элемента дерева Решение*/
 export const DECIS='DECIS';
+/**Тип элемента дерева Исполнение*/
 export const DECIS_ISPOLN='DECIS_ISPOLN';
+/**Тип элемента дерева */
 export const DEL_RAPORT='DEL_RAPORT';
